59b599d8ab2e57f5181b7a214b681f9443ed0bb9
hoatnnguyen <selenatn.nguyen@gmail.com>
2024-04-01 18:01:24 -0700

    clean code

--- backend/main.py
+++ backend/main.py
@@ -14,15 +14,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# def process_image(image_bytes):
-#     img = Image.open(io.BytesIO(image_bytes))
-#     # Example: Resize the image to 300x300
-#     # Should put logic to convert the image to sketch or anime styled image
-#     img.thumbnail((300, 300))
-#     output = io.BytesIO()
-#     img.save(output, format='JPEG')
-#     return output.getvalue()
-
 @app.get("/")
 async def root():
     return {"message": "SketPic"}
