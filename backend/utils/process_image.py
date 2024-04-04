29e2316daf65a9b7231310d39b5fc93b9ddec8f6
vanchuongpham <chapter95@gmail.com>
2024-04-03 23:11:25 -0700

    fixed encode

--- backend/utils/process_image.py
+++ backend/utils/process_image.py
@@ -6,13 +6,14 @@ import numpy as np
 import cv2
 
 def process_image(image_bytes):
+    
     img = Image.open(io.BytesIO(image_bytes))
     # Example: Resize the image to 300x300
     # Should put logic to convert the image to sketch or anime styled image
     img.thumbnail((300, 300))
     output = io.BytesIO()
     img.save(output, format='JPEG')
-    print(output.getvalue())
+    img.show()
     return output.getvalue()
 
 
@@ -21,13 +22,15 @@ def sketch_image(image_bytes):
     img = np.asarray(bytearray(image_bytes))
     img = cv2.imdecode(img, 1)
     sketched = sketch.PencilSketch()
-    result = sketched(img)
+    img = sketched(img)
 
-    cv2.imshow('img',result)
-    cv2.waitKey(0)
-    return result.tobytes()
+    _,result = cv2.imencode('.jpg',img)
+    result = result.tobytes()
+
+    return result
 
 #test picture
 url = "https://images.pexels.com/photos/236047/pexels-photo-236047.jpeg"
 img_bytes = requests.get(url).content
-sketch_image(img_bytes)
+test = sketch_image(img_bytes)
+process_image(test)
